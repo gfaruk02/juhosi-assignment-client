0e20b6189c3c02e70da613ff5ab95d45fcd07ab6
gfaruk02 <gfaruk02@gmail.com>
2024-02-08 22:43:42 +0600

    add heart

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -3,8 +3,12 @@
 const Header = () => {
     return (
         <> 
-        <img className="mask mask-heart" src="https://i.ibb.co/7tKrkX2/faruk-2.png" />
+<div className=" grid grid-cols-1 lg:grid-cols-3"> 
+<img className="mask mask-heart bg-rose-400 w-96" src="https://i.ibb.co/7tKrkX2/faruk-2.png" />
+<img className="mask mask-heart bg-rose-400 w-96" src="https://i.ibb.co/7tKrkX2/faruk-2.png" />
+<img className="mask mask-heart bg-rose-400 w-96" src="https://i.ibb.co/7tKrkX2/faruk-2.png" />
 
+</div>
 
         <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/WGMQ86v/d1.jpg)' }}>
             <div className="hero-overlay bg-opacity-60"></div>
