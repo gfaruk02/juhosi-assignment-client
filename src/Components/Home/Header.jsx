4c84a1927b031dbb27f0205d97e880c30a13f1a1
gfaruk02 <gfaruk02@gmail.com>
2024-02-09 23:43:11 +0600

    add some

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
                 <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold"> This is my Code</h1>
                     <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn btn-primary">Hire</button>
                 </div>
             </div>
         </div>
