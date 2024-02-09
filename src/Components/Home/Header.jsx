4557df7ae6abef0809c4d3253408771fe8ef8803
gfaruk02 <gfaruk02@gmail.com>
2024-02-09 23:46:01 +0600

    add modal in first page

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -16,7 +16,23 @@ const Header = () => {
                 <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold"> This is my Code</h1>
                     <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Hire</button>
+                    <button className="btn btn-primary">
+{/* Open the modal using document.getElementById('ID').showModal() method */}
+<button className="btn" onClick={()=>document.getElementById('my_modal_5').showModal()}>open modal</button>
+<dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">Hello!</h3>
+    <p className="py-4">Press ESC key or click the button below to close</p>
+    <div className="modal-action">
+      <form method="dialog">
+        {/* if there is a button in form, it will close the modal */}
+        <button className="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+
+                    </button>
                 </div>
             </div>
         </div>
